a02a97c635f4792a97f5f9e894a889c6cec756d5
QLC23 <qlc23@guest-wireless-upc-1602-10-120-038-082.usc.edu>
2018-02-25 08:29:39 -0800

    8:29 am push!

--- override.js
+++ override.js
@@ -5,3 +5,35 @@ function quote() {
     var hey = arr[num];
     document.getElementById("bub3").innerHTML = hey;
 }
+(function () {
+    var params = {
+        // Request parameters
+    };
+    $.ajax({
+        url: "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment?" + $.param(params),
+        beforeSend: function (xhrObj) {
+            // Request headers
+            xhrObj.setRequestHeader("Content-Type", "application/json");
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "{subscription key}");
+        },
+        type: "POST",
+        // Request body
+        data: {
+            "documents": [
+                {
+                    "language": "en",
+                    "id": 1,
+                    "text": browser.innerHTML
+                }
+            ]
+        }
+
+    })
+        .done(function (data) {
+            console.log(browser.innerHTML);
+            alert("success");
+        })
+        .fail(function () {
+            alert("error");
+        });
+});
