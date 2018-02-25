8ad4729f621d4e5baa0745d99cfbda3af999c6d2
QLC23 <qlc23@guest-wireless-upc-1602-10-120-038-082.usc.edu>
2018-02-25 11:50:05 -0800

    IT WORKS

--- override.js
+++ override.js
@@ -11,47 +11,43 @@ function quote() {
     var hey = arr[num];
     document.getElementById("bub3").innerHTML = hey;
 }
-$(function () {
-    var params = {
-        "documents": [
-            {
-                "language": "en",   
-                "id": 1,
-                "text": browse.value
-            }
-        ]
-    };
-    $.ajax({
-        url: "https://westcentralus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment?" + $.param(params),
-        beforeSend: function (xhrObj) {
-            // Request headers
-            xhrObj.setRequestHeader("Content-Type", "application/json");
-            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "8f7f16b82fd34fe4989932215f62c821");
-        },
-        type: "POST",
-        // Request body
-        // data:"{body}",
-        data: [
+document.getElementById("mag").addEventListener("click", mag);
+function mag(){
+    $(function () {
+        doc = JSON.stringify({
+            "documents": [
                 {
-                    "language": "en",
-                    "id": 1,
+                    "language": 'en',
+                    "id": '1',
                     "text": browse.value
                 }
             ]
-        }
 
-    )
-        .done(function (data) {
-            console.log(browse.value);
-            alert("success");
+
         })
-        .fail(function () {
-            alert("error");
-        });
-});
-document.getElementById("mag").addEventListener("click", mag);
-function mag(){
-    
+        $.ajax({
+
+            url: "https://westcentralus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment",
+            beforeSend: function (xhrObj) {
+                // Request headers
+                xhrObj.setRequestHeader("Content-Type", "application/json");
+                xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "8f7f16b82fd34fe4989932215f62c821");
+            },
+            type: "POST",
+            // Request body
+            // data:"{body}",
+            data: doc
+
+        })
+            .done(function (data) {
+                console.log(browse.value);
+                alert("success");
+                console.log(data["documents"][0]["score"]);
+            })
+            .fail(function () {
+                alert("error");
+            });
+    });
     var result = document.getElementById("browse").value;
     console.log(result);
     var gSearch = "https://www.google.com/search?q=" + result;
