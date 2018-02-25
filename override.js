db33c0ef1242b48d841e4286f2f430c91661cb1e
QLC23 <qlc23@guest-wireless-eup-1644-10-122-198-180.usc.edu>
2018-02-25 02:40:34 -0800

    finally it's kind of working...

--- override.js
+++ override.js
@@ -1,5 +1,7 @@
 document.getElementById("bub3").addEventListener("click", quote);
 function quote() {
-
-    document.getElementById("bub3").innerHTML = "WASSSUPPP!!";
+    var arr = ["You've got this!", "You gonna ace today!", "Make the most of your 86,400!!"];
+    var num = Math.floor((arr.length)*Math.random());
+    var hey = arr[num];
+    document.getElementById("bub3").innerHTML = hey;
 }
